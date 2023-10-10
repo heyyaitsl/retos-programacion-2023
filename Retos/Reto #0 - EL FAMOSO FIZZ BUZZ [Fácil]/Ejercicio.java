628e5a786466a7bff6fc718ae3678e94506841ff
laura <laurasarmiento1996@gmail.com>
2023-10-10 20:04:31 +0100

    Reto 0 recursivo

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/Ejercicio.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/Ejercicio.java
@@ -1,10 +1,21 @@
 public class Ejercicio {
     public static void main(String[] args){
 
-        for(int i=1; i<101;i++){
+      /*  for(int i=1; i<101;i++){
 
             String result = ((i%3==0) ? "fizz" : "" )+((i%5==0) ? "buzz" : "");
             System.out.println((result.isEmpty()) ? i : result);
+        }*/
+        recursividad(1);
+    }
+
+    private static void recursividad(int num){
+
+        String result = ((num%3==0) ? "fizz" : "" )+((num%5==0) ? "buzz" : "");
+        System.out.println((result.isEmpty()) ? num : result);
+
+        if (num<100){
+            recursividad(num+1);
         }
     }
 }
